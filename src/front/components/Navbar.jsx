19f9c47989640af135140c0daead4e7fedaa629e
Yarley Tique <arley.skn@gmail.com>
2025-03-26 20:35:05 +0000

    modificado color y se añidio el logo

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,13 +3,28 @@ import { Navbar, Nav, Container, Form, FormControl, Button } from "react-bootstr
 import { Link } from "react-router-dom";
 
 const NavbarProject = () => {
+	const logoUrl = "https://i.imgur.com/CckqetR.png";
 	return (
-		<Navbar bg="light" expand="lg" sticky="top">
+		<Navbar bg="info-subtle" expand="lg" sticky="top">
 			<Container>
 				{/*Que, Como y cuando*/}
-				<Navbar.Brand as={Link} to="/" className="fw-bold text-success">
-					<i classname="fas fa-utensils me-2"></i>
+				<Navbar.Brand as={Link} to="/" className="d-flex align-items-center" style={{height:"60px"}}>
+					<img src={logoUrl}
+					alt="Logo"
+					style={{
+					width:"100px",
+					height:"auto",
+					maxHeight:"200%",
+					objectFit:"contain"
+					}}
+					classname="me-2"
+					onError={(e) =>{ e.target.onerror = null;
+						e.target.src = "https://via.placeholder.com/40";
+					 }}
+					 />
+				<span className="fw-bold text-info fs-4 ">
 					Que, Como y Cuando
+				</span>	
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
@@ -32,7 +47,7 @@ const NavbarProject = () => {
 						placeholder="Buscar Recetas..."
 						className="me-2"
 						aria-label="Search"/>
-						<Button variant="outline-success">
+						<Button variant="outline-info">
 							<i className="fas fa-search"></i>
 						</Button>
 					</Form>
@@ -40,14 +55,14 @@ const NavbarProject = () => {
 					{/*inicio de sesión*/}
 					<div>
 						<Button
-							variant="outline-primary"
+							variant="outline-info"
 							className="me-2"
 							as={Link}
 							to="/login" >
 								Iniciar Sesión
 							</Button>
 							<Button 
-							variant="success"
+							variant="light"
 							as={Link}
 							to="/register">
 								Registrarse
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react"
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
@@ -34,9 +33,14 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
+			<h1 className="display-4 ">Hello Estamos en construcción!!</h1>
 			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
+				<img
+				style={{
+					width:"400px",
+					height:"400px",						
+					}}
+				src="https://i.imgur.com/CckqetR.png" className="img-fluid rounded-circle mb-3 " alt="Rigo Baby" />
 			</p>
 			<div className="alert alert-info">
 				{store.message ? (
